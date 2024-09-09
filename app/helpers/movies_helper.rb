c01dd0606d2f3697052f385c6de74e80c10fc3b9
jingruimao <vivinmao@tamu.edu>
2024-09-09 16:31:58 -0500

    1

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,14 +1,20 @@
 module MoviesHelper
+    # def sortable(column, title = nil)
+    #     title ||= column.titleize
+    #     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+    #     link_to title, sort: column, direction: direction
+    #   end
+    def sort_indicator(column)
+      if params[:sort] == column
+        params[:direction] == "asc" ? "↑" : "↓"
+      end
+    end
     def sortable(column, title = nil)
-        title ||= column.titleize
-        direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-        link_to title, sort: column, direction: direction
-      end
-      def sort_indicator(column)
-        if params[:sort] == column
-          params[:direction] == "asc" ? "↑" : "↓"
-        else
-          ""
-        end
-      end
+      title ||= column.titleize
+      direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+      link_to title, { sort: column, direction: direction }, class: "sort-link"
+    end
+    def sort_class(column)
+      params[:sort] == column ? params[:direction] : ""
+    end
 end
